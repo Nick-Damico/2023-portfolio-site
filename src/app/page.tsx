d405f10b3a20a1d107d2299a96337b069ee55bb1
Nick-Damico <nickalan82@icloud.com>
2023-08-06 08:46:38 -0400

    Refactor: Rename function to Landing; Moved Home page content to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import Header from '@/components/header'
 import Home from '@/components/home'
 import Footer from '@/components/footer'
+
+export default function Landing() {
   return (
+    <div className='box-border flex min-h-screen flex-col items-center justify-between'>
+      <div className='max-w-screen-xl w-full h-100 grow bg-zinc-800'>
+        {/* <Image
+          className='absolute'
+          src='/design-image01.jpg'
+          width='500'
+          height='1024'
+          alt=''
+        /> */}
         <Header />
         <Home />
         <Footer />
