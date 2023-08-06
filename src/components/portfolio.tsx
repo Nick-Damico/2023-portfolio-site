472a654a8a13e5d7d1bab6d4a4353e29c145cf2e
Nick-Damico <nickalan82@icloud.com>
2023-08-06 16:32:45 -0400

    Refactor: Adjust Portfolio content and styling

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,7 +1,21 @@
-export default function Portfolio() {
+export default function Portfolio({ title }: { title: string }) {
   return (
-    <section className='w-100 md:w-1/2 px-4 mt-8'>
-      <h2>Portfolio</h2>
+    <section className='w-full mb-6'>
+      <h2 className='uppercase text-2xl font-thin'>{title}</h2>
+      <div className='box-border flex w-full flex-wrap'>
+        <div className='flex justify-center items-center w-1/2 h-[100px] bg-emerald-500'>
+          Front-End
+        </div>
+        <div className='flex justify-center items-center w-1/2 h-[100px] bg-emerald-500'>
+          Ruby on Rails
+        </div>
+        <div className='flex justify-center items-center w-1/2 h-[100px] bg-emerald-500'>
+          Ruby
+        </div>
+        <div className='flex justify-center items-center w-1/2 h-[100px] bg-emerald-500'>
+          TypeScript
+        </div>
+      </div>
     </section>
   )
 }
