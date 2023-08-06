bf2ac93ff7b7a7eb1efa55433337b4639e0418ca
Nick-Damico <nickalan82@icloud.com>
2023-08-06 08:44:58 -0400

    Feat: Add component Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Header from '@/components/header'
 import Home from '@/components/home'
+import Footer from '@/components/footer'
   return (
         <Header />
+        <Home />
+        <Footer />
       </div>
     </div>
   )
