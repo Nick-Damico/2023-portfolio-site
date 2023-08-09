3b255b0f4ce11bfe643bfc7aab80eb0ea1b7eaf8
Nick-Damico <nickalan82@icloud.com>
2023-08-09 12:04:11 -0400

    Feat: Add skills component; Adjust layout for Skills component placement

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,44 +1,49 @@
 import Timeline from '@/components/timeline'
 import Portfolio from '@/components/portfolio'
 import Blog from '@/components/blog'
+import Skills from '@/components/skills'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className='flex flex-col mt-28 mb-auto px-4'>
-      <div className='relative max-w-sm md:max-w-md'>
-        <div className='flex flex-col'>
-          <h1 className='mt-10 text-6xl text-slate-100 text-700 font-mont font-medium z-50'>
-            Nick D&apos;Amico
-          </h1>
-          <span className='self-end text-sm text-emerald-400 uppercase font-mont z-50'>
-            Developer &bull; Ruby on Rails &bull; JavaScript
-          </span>
+      <div className='grid grid-cols-2'>
+        <div className='relative col-span-2 max-w-sm md:max-w-md'>
+          <div className='flex flex-col'>
+            <h1 className='mt-10 text-6xl text-slate-100 text-700 font-mont font-medium z-50'>
+              Nick D&apos;Amico
+            </h1>
+            <span className='self-end text-sm text-emerald-400 uppercase font-mont z-50'>
+              Developer &bull; Ruby on Rails &bull; JavaScript
+            </span>
+          </div>
+          <Image
+            className='absolute rounded-full z-10 -scale-x-100 -top-10 right-0 md:-right-24 md:-top-20 border-4 border-emerald-400'
+            src='/profile.jpg'
+            alt='Profile picture of Nick Damico'
+            width={180}
+            height={180}
+          />
         </div>
-        <Image
-          className='absolute rounded-full z-10 -scale-x-100 -top-10 right-0 md:-right-24 md:-top-20 border-4 border-emerald-400'
-          src='/profile.jpg'
-          alt='Profile picture of Nick Damico'
-          width={180}
-          height={180}
-        />
-      </div>
 
-      {/* TODO: add lg:-mb-12 back after skills section is complete */}
-      <section className='flex flex-col w-100 max-w-md mb-20'>
-        <h2 className='ml-32 text-lg text-black'></h2>
-        <p className='mt-8 uppercase'>
-          I&apos;m a software developer from East Tennessee with{' '}
-          <em>
-            4+ years of professional{' '}
-            <span className='text-rose-600'> Ruby on Rails</span> &amp;{' '}
-            <span className='text-amber-400'>JavaScript</span> experience{' '}
-          </em>
-          working in the fashion / appareal industry at Tiltworks, Inc. and a
-          background in the liqudation of new car dealership fixed assets at
-          auction.
-        </p>
-      </section>
+        {/* TODO: add lg:-mb-12 back after skills section is complete */}
+        <section className='flex flex-col w-100 max-w-md mb-20 col-span-2 md:col-span-1'>
+          <h2 className='ml-32 text-lg text-black'></h2>
+          <p className='mt-8 uppercase'>
+            I&apos;m a software developer from East Tennessee with{' '}
+            <em>
+              4+ years of professional{' '}
+              <span className='text-rose-600'> Ruby on Rails</span> &amp;{' '}
+              <span className='text-amber-400'>JavaScript</span> experience{' '}
+            </em>
+            working in the fashion / appareal industry at Tiltworks, Inc. and a
+            background in the liqudation of new car dealership fixed assets at
+            auction.
+          </p>
+        </section>
+
+        <Skills title='Skills' />
+      </div>
 
       <div className='grid grid-cols-2 gap-2'>
         <Timeline title='Timeline' />
---
+++ src/components/skills.tsx
@@ -0,0 +1,135 @@
+import Image from 'next/image'
+import { ReactNode } from 'react'
+
+export default function Skills({ title }: { title: string }) {
+  const skills = {
+    languages: [
+      {
+        filename: 'html5-original.svg',
+        altText: 'Hyper Text Markup lanugage 5'
+      },
+      {
+        filename: 'ruby-plain.svg',
+        altText: 'Ruby programming language'
+      },
+      {
+        filename: 'javascript-original.svg',
+        altText: 'Javascript programming language'
+      },
+      {
+        filename: 'typescript-plain.svg',
+        altText: 'Typescript programming language'
+      }
+    ],
+    frameworks: [
+      {
+        filename: 'rails-plain.svg',
+        altText: 'Ruby on Rails'
+      },
+      {
+        filename: 'nextjs-original.svg',
+        altText: 'Next js'
+      },
+      {
+        filename: 'react-original.svg',
+        altText: 'React js'
+      }
+    ],
+    styling: [
+      {
+        filename: 'css3-original.svg',
+        altText: 'Cascading Style Sheets 3'
+      },
+      {
+        filename: 'bootstrap-plain.svg',
+        altText: 'Bootstrap'
+      },
+      {
+        filename: 'tailwindcss-plain.svg',
+        altText: 'Tailwindcss'
+      }
+    ],
+    misc: [
+      {
+        filename: 'rspec-original.svg',
+        altText: 'RSpec testing framework'
+      },
+      {
+        filename: 'docker-original.svg',
+        altText: 'Docker'
+      },
+      {
+        filename: 'github-original.svg',
+        altText: 'Github'
+      },
+      {
+        filename: 'gitlab-original.svg',
+        altText: 'Gitlab'
+      }
+    ]
+  }
+
+  return (
+    <section className='flex flex-col col-span-2 md:col-span-1'>
+      <h2 className='section-title'>{title}</h2>
+      <div className='grid grid-cols-4 gap-2'>
+        <div className='flex flex-col col-start-1 col-span-1 items-center'>
+          {skills.languages.map(
+            ({ filename, altText }, idx): ReactNode => (
+              <Image
+                key={idx}
+                className='mb-2'
+                src={`/${filename}`}
+                alt={altText}
+                width={40}
+                height={40}
+              />
+            )
+          )}
+        </div>
+        <div className='flex flex-col col-start-2 col-span-1 items-center'>
+          {skills.styling.map(
+            ({ filename, altText }, idx): ReactNode => (
+              <Image
+                key={idx}
+                className='mb-2'
+                src={`/${filename}`}
+                alt={altText}
+                width={40}
+                height={40}
+              />
+            )
+          )}
+        </div>
+        <div className='flex flex-col col-start-3 col-span-1 items-center'>
+          {skills.frameworks.map(
+            ({ filename, altText }, idx): ReactNode => (
+              <Image
+                key={idx}
+                className='mb-2'
+                src={`/${filename}`}
+                alt={altText}
+                width={40}
+                height={40}
+              />
+            )
+          )}
+        </div>
+        <div className='flex flex-col col-start-4 col-span-1 items-center'>
+          {skills.misc.map(
+            ({ filename, altText }, idx): ReactNode => (
+              <Image
+                key={idx}
+                className='mb-2'
+                src={`/${filename}`}
+                alt={altText}
+                width={40}
+                height={40}
+              />
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  )
+}
