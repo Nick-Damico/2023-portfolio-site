745024102f645d497e96d0536afd49d5f7ee76d8
Nick-Damico <nickalan82@icloud.com>
2023-08-09 10:04:38 -0400

    Refactor: Add timeline events data-structure to DRY up repeated code

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,30 +1,58 @@
+import { ReactNode } from 'react'
+
 export default function Timeline({ title }: { title: string }) {
+  const events = [
+    {
+      startDate: 'Apr 2019',
+      endDate: 'June 2023',
+      company: 'Tiltworks, Inc',
+      title: 'Software Engineer'
+    },
+    {
+      startDate: 'Mar 2017',
+      endDate: 'Jan 2018',
+      company: 'Flatiron Bootcamp',
+      title: 'Software Engineering'
+    },
+    {
+      startDate: 'Sept 2016',
+      endDate: 'Apr 2017',
+      company: 'Teamtreehouse.com',
+      title: 'Front-End Techdegree'
+    },
+    {
+      startDate: 'Jan 2008',
+      endDate: 'Mar 2019',
+      company: 'Damicoauctions.com',
+      title: 'Licensed Auctioneer'
+    },
+    {
+      startDate: 'Sept 2006',
+      endDate: 'May 2007',
+      company: 'Walters State CC',
+      title: 'Technical Certification EMT-IV'
+    }
+  ]
+
   return (
     <section className='col-span-2 md:col-start-2 md:col-span-1 mb-6'>
       <h2 className='section-title'>{title}</h2>
       <ol className='space-y-2 border-l-2 border-emerald-400'>
-        <li className='relative ml-3 before:absolute before:content-[""] before:w-3 before:h-3 before:rounded-full before:top-4 before:-left-[1.2rem] before:bg-emerald-400'>
-          <span className='block font-bold text-rose-600'>
-            Apr 2019 - June 2023
-          </span>
-          Tiltworks, Inc - <i>Software Engineer</i>
-        </li>
-        <li className='relative ml-3 before:absolute before:content-[""] before:w-3 before:h-3 before:rounded-full before:top-4 before:-left-[1.2rem] before:bg-emerald-400'>
-          <span className='block font-bold text-rose-600'>
-            Mar 2018 - Dec 2018
-          </span>
-          Flatiron Bootcamp - <i>Software Engineering</i>
-        </li>
-        <li className='relative ml-3 before:absolute before:content-[""] before:w-3 before:h-3 before:rounded-full before:top-4 before:-left-[1.2rem] before:bg-emerald-400'>
-          <span className='block font-bold text-rose-600'>
-            Mar 2017 - Sept 2017
-          </span>
-          Teamtreehouse.com - <i>Front-End Techdegree</i>
-        </li>
-        <li className='relative ml-3 before:absolute before:content-[""] before:w-3 before:h-3 before:rounded-full before:top-4 before:-left-[1.2rem] before:bg-emerald-400'>
-          <span className='block font-bold text-rose-600'>2007 - 2019</span>
-          DamicoAuctions.com - <i>Licensed Auctioneer</i>
-        </li>
+        {events.map(
+          ({ startDate, endDate, company, title }, i): ReactNode => (
+            <li
+              key={i}
+              className='relative ml-3 before:absolute before:content-[""] before:w-3 before:h-3 before:rounded-full before:top-4 before:-left-[19px] before:bg-emerald-400'
+            >
+              <span className='block font-bold text-rose-600 -mb-1'>
+                {startDate} - {endDate}
+              </span>
+              <p className='ml-1'>
+                {company} - <i>{title}</i>
+              </p>
+            </li>
+          )
+        )}
       </ol>
     </section>
   )
