c956c4672ae37d8422b90f09a157673ce94515eb
Nick-Damico <nickalan82@icloud.com>
2023-08-09 10:24:33 -0400

    Feat: Add gradient background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Footer from '@/components/footer'
 
 export default function Landing() {
   return (
-    <div className='box-border flex min-h-screen flex-col items-center justify-between bg-zinc-800'>
+    <div className='box-border flex min-h-screen flex-col items-center justify-between bg-gradient-to-r from-zinc-800 to-stone-950'>
       <div className='flex flex-col max-w-screen-xl w-full h-100 grow'>
         <Header />
         <Home />
