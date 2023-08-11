5661dc70ac7f64bd83db6aca185121a45e279609
Nick-Damico <nickalan82@icloud.com>
2023-08-11 11:43:22 -0400

    Refactor: Move h1 and about infor to MainHeading and About components

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <section className='flex flex-col w-100 max-w-md mb-20'>
+      <h2 className='ml-32 text-lg text-black'></h2>
+      <p className='mt-8 uppercase pr-4'>
+        I&apos;m a software developer from East Tennessee with{' '}
+        <em>
+          4+ years of professional{' '}
+          <span className='text-rose-600'> Ruby on Rails</span> &amp;{' '}
+          <span className='text-amber-400'>JavaScript</span> experience{' '}
+        </em>
+        working in the fashion / appareal industry at Tiltworks, Inc. and a
+        background in the liqudation of new car dealership fixed assets at
+        auction.
+      </p>
+    </section>
+  )
+}
---
+++ src/components/MainHeading.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+export default function MainHeading() {
+  return (
+    <div className='relative max-w-sm sm:max-w-md md:max-w-md'>
+      <div className='flex flex-col'>
+        <h1 className='mt-10 text-6xl sm:text-slate-100 text-700 font-mont font-medium z-50'>
+          Nick D&apos;Amico
+        </h1>
+        <span className='self-end text-sm text-emerald-400 uppercase font-mont z-50'>
+          Developer &bull; Ruby on Rails &bull; JavaScript
+        </span>
+      </div>
+      <Image
+        className='absolute rounded-full z-10 -scale-x-100 -top-12 sm:-top-20 md:-top-20 right-0 sm:-right-32 md:-right-32 border-4 border-emerald-400'
+        src='/profile.jpg'
+        alt='Profile picture of Nick Damico'
+        width={180}
+        height={180}
+      />
+    </div>
+  )
+}
