fe977f0d71c4f055cd94fb61b07d153a5bdda18a
Nick-Damico <nickalan82@icloud.com>
2023-08-21 16:51:44 -0400

    Refactor: Build ProjectItem's with list data

--- src/components/portfolio/ProjectList.tsx
+++ src/components/portfolio/ProjectList.tsx
@@ -1,13 +1,16 @@
 import ProjectItem from './ProjectItem'
-import Modal from '@/components/Modal'
+import projects, { Project } from './data'
 
 export default function ProjectList() {
   return (
-    <div className='group grid gap-1 grid-cols-2 grid-rows-2 box-border h-80'>
-      <ProjectItem name='Front-End' img='/hikeit-app-full.jpg' />
-      <ProjectItem name='Ruby On Rails' img='/shear-madness.jpg' />
-      <ProjectItem name='Ruby' img='/damico-auctions.jpg' />
-      <Modal />
+      {projects.map(({ id, name, src }) => (
+        <ProjectItem
+          key={id}
+          name={name}
+          img={src}
+          onClick={renderProjectInModal}
+        />
+      ))}
     </div>
   )
 }
