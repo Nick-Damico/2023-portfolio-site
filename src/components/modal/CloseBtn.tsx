28e2f3098eee3b7202bbfeb05e9a44a0c3c6e19d
Nick-Damico <nickalan82@icloud.com>
2023-08-24 09:40:35 -0400

    Feat(UI): Adjust Close button

--- src/components/modal/CloseBtn.tsx
+++ src/components/modal/CloseBtn.tsx
@@ -7,13 +7,19 @@ export default function CloseBtn({ onClick }: { onClick: Function }) {
   }
 
   return (
-    <div className='flex justify-end'>
+    <div className='flex justify-end mb-4'>
       <button
         onClick={handleClick}
         type='button'
-        className='flex w-8 h-8 rounded-full justify-center items-center bg-white font-bold text-xl'
+        className='flex w-6 h-6 rounded-full justify-center items-center bg-white font-bold text-xl'
       >
-        <Image className='p-1' src={xIcon} width={28} height={28} alt='' />
+        <Image
+          className='p-1 rounded'
+          src={xIcon}
+          width={28}
+          height={28}
+          alt=''
+        />
       </button>
     </div>
   )
