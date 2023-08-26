0f7b38687937e402b2069fa35482b6b6fe0409a6
Nick-Damico <nickalan82@icloud.com>
2023-08-26 14:28:43 -0400

    Chore: Updated Tech icon map; Updated Portfolio Content

--- src/components/portfolio/data.tsx
+++ src/components/portfolio/data.tsx
@@ -15,11 +15,19 @@ type TechIconMap = {
 export const techIcons: TechIconMap = {
   bootstrap:
     'https://img.shields.io/badge/Bootstrap-834899?style=flat-square&logo=bootstrap&logoColor=white',
+  css: 'https://img.shields.io/badge/CSS3-1572B6?style=flat-square&logo=css3&logoColor=white',
+  gatsby:
+    'https://img.shields.io/badge/Gatsby-663399?style=flat-square&logo=gatsby&logoColor=white',
+  netlify:
+    'https://img.shields.io/badge/Netlify-00C7B7?style=flat-square&logo=netlify&logoColor=white',
   ruby: 'https://img.shields.io/badge/Ruby-2356752?style=flat-square&logo=ruby&color=c93c32',
   rails:
     'https://img.shields.io/badge/Ruby_on_Rails-d13232?style=flat-square&logo=rubyonrails&color=d13232',
   javascript:
-    'https://img.shields.io/badge/JavaScript-2e2d2d?style=flat-square&logo=javascript&color=2e2d2d'
+    'https://img.shields.io/badge/JavaScript-2e2d2d?style=flat-square&logo=javascript&color=2e2d2d',
+  php: 'https://img.shields.io/badge/PHP-777BB4?style=flat-square&logo=php&logoColor=white',
+  wordpress:
+    'https://img.shields.io/badge/Wordpress-21759B?style=flat-square&logo=wordpress'
 }
 
 const projects: Project[] = [
@@ -41,15 +49,24 @@ const projects: Project[] = [
     id: '1480b6a7-3582-45b7-9a9c-464db1fef357',
     name: 'Gatsby JS',
     src: '/shear-madness.jpg',
-    techList: ['ruby', 'javascript', 'rails'],
-    info: ``
+    techList: ['gatsby', 'javascript', 'css'],
+    info: `I was contracted by a local hair salon in Sevier County Shear Madness to
+    build a website to attract more clientele. The owners had an outdated Wordpress website
+    that was not ranking well on Google, and they feared the outdated site hurt business.
+    I worked closely to build a site that matched their style, displayed their talent, and drive 
+    new business.`
   },
   {
     id: '0d588fc9-f621-4921-9ec5-df9e894f656f',
     name: 'Wordpress',
     src: '/damico-auctions.jpg',
-    techList: ['ruby', 'javascript', 'rails'],
-    info: ``
+    techList: ['wordpress', 'php', 'javascript', 'css'],
+    info: `
+      DamicoAuctions.com is the offical website for my families auction company.
+      We specialize in the appraisal and liquidation of automotive equipment and new car dealerships.
+      This is a fully custom Wordpress website with custom auction events. It was built using PHP, JavaScript, and CSS. The 
+    `,
+    githubSrc: ''
   }
 ]
 
