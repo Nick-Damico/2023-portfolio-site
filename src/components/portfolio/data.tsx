08fc6dce33010893cda7aee04c1acb6dff9ed3e1
Nick-Damico <nickalan82@icloud.com>
2023-09-09 12:59:02 -0400

    Feat: Update Portfolio Project data

--- src/components/portfolio/data.tsx
+++ src/components/portfolio/data.tsx
@@ -43,7 +43,15 @@ const projects: Project[] = [
     id: 'db7f444a-86d1-4351-8b46-817c19087f43',
     name: 'HikeIt - Social Hiking Meetup App',
     imgSrc: '/hikeit-app-full.jpg',
-    techList: ['ruby', 'rails', 'javascript', 'bootstrap', 'rspec'],
+    techList: [
+      'ruby',
+      'rails',
+      'javascript',
+      'bootstrap',
+      'rspec',
+      'github',
+      'html'
+    ],
     info: `
       HikeIt is a social meetup application for organizing
       hikes witin the Smoky Mountain Naitonal Park. This application
@@ -56,38 +64,80 @@ const projects: Project[] = [
     ]
   },
   {
-    id: '1480b6a7-3582-45b7-9a9c-464db1fef357',
-    name: 'Gatsby JS',
-    imgSrc: '/shear-madness.jpg',
-    techList: ['gatsby', 'javascript', 'css'],
-    info: `I was contracted by a local hair salon in Sevier County Shear Madness to
-    build a website to attract more clientele. The owners had an outdated Wordpress website
-    that was not ranking well on Google, and they feared the outdated site hurt business.
-    I worked closely to build a site that matched their style, displayed their talent, and drive new business.`
+    id: 'd09d4cd3-900e-4302-ace8-66d21e70d028',
+    name: 'Responsive Image Gallery',
+    imgSrc: '/inter-photo-full.jpg',
+    techList: ['html', 'css', 'javascript', 'github'],
+    info: `
+      This project was part of the Treehouse Front End Techdegree program.
+      The objective of this project was to use HTML5 and CSS to build a responsive photo gallery.
+      JavaScript was utilitized to add a modal pop-up and image carousel with text search filtering.
+    `,
+    links: [
+      {
+        type: 'github',
+        href: 'https://github.com/Nick-Damico/Proj.-4-Interactive-Photo-Gallery'
+      },
+      {
+        type: 'live',
+        href: 'https://nick-damico.github.io/Proj.-4-Interactive-Photo-Gallery/'
+      }
+    ]
   },
   {
-    id: '0d588fc9-f621-4921-9ec5-df9e894f656f',
-    name: 'Wordpress',
-    imgSrc: '/damico-auctions.jpg',
-    techList: ['wordpress', 'php', 'javascript', 'css'],
+    id: '9fae7a83-e40e-41f1-99e1-b67eff891058',
+    name: 'Interactive Video Player',
+    imgSrc: '/video-player-full.jpg',
+    techList: ['html', 'css', 'javascript', 'github'],
     info: `
-      DamicoAuctions.com is the offical website for my families auction company.
-      We specialize in the appraisal and liquidation of automotive equipment and new car dealerships.
-      This is a fully custom Wordpress website with custom auction events. It was built using PHP, JavaScript, and CSS. The 
-    `
-  },
-  {
-    id: '',
-    name: 'MasterMind CLI Game',
-    imgSrc: '',
-    techList: ['ruby', 'rspec'],
-    info: `
-      Master Mind CLI is a command-line interface (CLI) implementation of the popular
-      game Master Mind.  Test your code-breaking skills by attempting to guess the
-      secret pattern within the specified number of attempts. Have fun playing and mastering
-      the code-breaking challenge with Master Mind CLI!
-    `
+      This project showcases an HTML5 video player built using JavaScript and the 
+      HTML5 Video API. The goal of this project was to create an interactive video 
+      player with custom controls that synchronizes the video playback with a transcript.
+    `,
+    links: [
+      {
+        type: 'github',
+        href: 'https://github.com/Nick-Damico/Proj.-7-Interactive-Video-Player'
+      },
+      {
+        type: 'live',
+        href: 'https://nick-damico.github.io/Proj.-7-Interactive-Video-Player'
+      }
+    ]
   }
+  // {
+  //   id: '1480b6a7-3582-45b7-9a9c-464db1fef357',
+  //   name: 'Gatsby JS',
+  //   imgSrc: '/shear-madness.jpg',
+  //   techList: ['gatsby', 'javascript', 'css'],
+  //   info: `I was contracted by a local hair salon in Sevier County Shear Madness to
+  //   build a website to attract more clientele. The owners had an outdated Wordpress website
+  //   that was not ranking well on Google, and they feared the outdated site hurt business.
+  //   I worked closely to build a site that matched their style, displayed their talent, and drive new business.`
+  // },
+  // {
+  //   id: '0d588fc9-f621-4921-9ec5-df9e894f656f',
+  //   name: 'Wordpress',
+  //   imgSrc: '/damico-auctions.jpg',
+  //   techList: ['wordpress', 'php', 'javascript', 'css'],
+  //   info: `
+  //     DamicoAuctions.com is the offical website for my families auction company.
+  //     We specialize in the appraisal and liquidation of automotive equipment and new car dealerships.
+  //     This is a fully custom Wordpress website with custom auction events. It was built using PHP, JavaScript, and CSS. The
+  //   `
+  // },
+  // {
+  //   id: '',
+  //   name: 'MasterMind CLI Game',
+  //   imgSrc: '',
+  //   techList: ['ruby', 'rspec'],
+  //   info: `
+  //     Master Mind CLI is a command-line interface (CLI) implementation of the popular
+  //     game Master Mind.  Test your code-breaking skills by attempting to guess the
+  //     secret pattern within the specified number of attempts. Have fun playing and mastering
+  //     the code-breaking challenge with Master Mind CLI!
+  //   `
+  // }
 ]
 
 export default projects
